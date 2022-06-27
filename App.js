5c4332b2c946ec2acfe377c1115973f31f3c0035
Cynthia <cynbernardes@gmail.com>
2022-06-27 17:56:54 -0300

    .

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+ import styles from './styles.js';
+import { ContadorRegressivo } from './src/components/ContadorRegressivo.js';
+import { Flex1 } from './src/flex/exe1/index.js';
+import { Flex2 } from './src/flex/exe2/index.js';
+import { Flex3 } from './src/flex/exe3/index.js';
+import { Flex4 } from './src/flex/exe4/index.js';
+import { Flex5 } from './src/flex/exe5/index.js';
+import { Flex6 } from './src/flex/exe6/index.js';
+import { Desafio01 } from './src/desafio01/index.js';
+
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  //  <ContadorRegressivo />
+    //  <Flex1 />
+    // <Flex2 />
+    // <Flex3 />
+    // <Flex4 />
+    // <Flex5 />
+    // <Flex6 />
+     <Desafio01 />
+
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native'
+
+ export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'lightgray'
+    },
+
+    titulo: {
+        fontSize: 40
+    },
+
+    contador: {
+        fontSize: 100,
+        marginBottom: 20
+    },
+    valorInicial: {
+        marginTop: 20,
+        textAlign: 'center'
+    },
+    
+    input: {
+        backgroundColor: 'white',
+        marginBottom: 10
+    }
+});
