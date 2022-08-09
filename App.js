21c2b578062b2e8660357aa5e7c6005dc1e64858
Cynthia <cynbernardes@gmail.com>
2022-08-09 14:28:43 -0300

    'Exercícios Flex Box'

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
- import styles from './styles.js';
-import { ContadorRegressivo } from './src/components/ContadorRegressivo.js';
 import { Flex1 } from './src/flex/exe1/index.js';
 import { Flex2 } from './src/flex/exe2/index.js';
 import { Flex3 } from './src/flex/exe3/index.js';
@@ -13,14 +11,13 @@ import { Desafio01 } from './src/desafio01/index.js';
 
 export default function App() {
   return (
-  //  <ContadorRegressivo />
-    //  <Flex1 />
+    // <Flex1 />
     // <Flex2 />
     // <Flex3 />
     // <Flex4 />
     // <Flex5 />
     // <Flex6 />
-     <Desafio01 />
+      <Desafio01 />
 
 
   );
